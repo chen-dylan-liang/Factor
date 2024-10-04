7665a03b4630124be3caf3f13fc107600afad8d4
ChenDylanLIANG <lcwater98@gmail.com>
2024-10-03 20:40:14 -0400

    add check force sensor config

--- collect_data.py
+++ collect_data.py
@@ -31,14 +31,60 @@ arm.motion_enable(enable=True)
 arm.ft_sensor_enable(0)
 arm.set_mode(0)
 arm.set_state(state=0)
-arm.set_position(x=496.2, y=-28.8, z=469.4, roll=51.7, pitch=-85.9, yaw=125.6, wait=True)
 time.sleep(0.5)
+arm.set_position(x=496.2, y=-28.8, z=469.4, roll=51.7, pitch=-85.9, yaw=125.6, wait=True)
+
+
+def safe_exit(code):
+    # turn off manual mode after recording
+    arm.set_mode(0)
+    arm.set_state(0)
+
+    # turn off force sensor
+    arm.ft_sensor_enable(0)
+
+    # disconnect after use
+    arm.disconnect()
+    if code == 0:
+        exit(0)
+    else:
+        exit(1)
+
 
 # turn on the force sensor
 arm.ft_sensor_enable(1)
 arm.ft_sensor_set_zero()
 arm.ft_sensor_app_set(0)
 time.sleep(0.5)
+see_ft_sensor_config = True
+if see_ft_sensor_config:
+    code, config = arm.get_ft_senfor_config()
+    if code == 0:
+        print('ft_app_status: {}'.format(config[0]))
+        print('ft_is_started: {}'.format(config[1]))
+        print('ft_type: {}'.format(config[2]))
+        print('ft_id: {}'.format(config[3]))
+        print('ft_freq: {}'.format(config[4]))
+        print('ft_mass: {}'.format(config[5]))
+        print('ft_dir_bias: {}'.format(config[6]))
+        print('ft_centroid: {}'.format(config[7]))
+        print('ft_zero: {}'.format(config[8]))
+        print('imp_coord: {}'.format(config[9]))
+        print('imp_c_axis: {}'.format(config[10]))
+        print('M: {}'.format(config[11]))
+        print('K: {}'.format(config[12]))
+        print('B: {}'.format(config[13]))
+        print('f_coord: {}'.format(config[14]))
+        print('f_c_axis: {}'.format(config[15]))
+        print('f_ref: {}'.format(config[16]))
+        print('f_limits: {}'.format(config[17]))
+        print('kp: {}'.format(config[18]))
+        print('ki: {}'.format(config[19]))
+        print('kd: {}'.format(config[20]))
+        print('xe_limit: {}'.format(config[21]))
+    else:
+        print("Error in checking force sensor config!!!")
+        safe_exit(code)
 
 # turn on teach mode
 arm.set_mode(2)
@@ -56,28 +102,28 @@ while arm.connected and arm.error_code == 0:
         data['pos_data'].append(pos)
     else:
         print("Error in position data!!!")
-        break
+        safe_exit(code)
 
     code, pos_aa = arm.get_position_aa()
     if code == 0:
         data['pos_aa_data'].append(pos_aa)
     else:
         print("Error in position axis angle data!!!")
-        break
+        safe_exit(code)
 
     code, js = arm.get_joint_states()
     if code == 0:
         data['joint_state_data'].append(js)
     else:
         print("Error in joint state data!!!")
-        break
+        safe_exit(code)
 
     code, ext_force = arm.get_ft_sensor_data()
     if code == 0:
         data['ext_f_data'].append(ext_force)
     else:
         print("Error in force data!!!")
-        break
+        safe_exit(code)
 
     raw_force = arm.ft_raw_force
     data['raw_f_data'].append(raw_force)
@@ -98,12 +144,4 @@ with open(traj_name + ".traj", 'wb') as file:
     pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
 time.sleep(1)
 
-# turn off manual mode after recording
-arm.set_mode(0)
-arm.set_state(0)
-
-# turn off force sensor
-arm.ft_sensor_enable(0)
-
-# disconnect after use
-arm.disconnect()
+safe_exit(0)
