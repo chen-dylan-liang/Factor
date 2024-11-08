fix the random rotation bug

deploy_model.py
@@ -1,9 +1,10 @@
 import sys, os
-from collect_data import initialize_arm, set_to_init_pos, turn_on_force_sensor, safe_exit
+from collect_data import initialize_arm, set_to_init_pos, turn_on_force_sensor, safe_exit, enable_online_mode
 import torch as th
 from model import FeedForwardModel
 import time
 import keyboard
+import numpy as np
 
 
 def process_argv():
@@ -48,11 +49,16 @@ def deploy_model(_arm, _model, _dur=10, _look_ahead=5, _print_out=False):
             _code_p, _pos = _arm.get_position()
             _code_f, _force = _arm.get_ft_sensor_data()
             _future_pos = [float(x) for x in _model(th.tensor(_pos + _force, dtype=th.float32))]  # 10*6
-            _arm.set_position(*_future_pos[6*(_look_ahead-1):6*_look_ahead], speed=50, wait=True)
+            _new_signal = np.array(_future_pos[0:6 * _look_ahead]).reshape(-1, 6)
+            _new_signal = np.mean(_new_signal, axis=0)
+            _control_signal = _new_signal
+            _arm.set_position(*(list(_control_signal[0:3])), speed=80, wait=True)
             end = time.time()
             if _print_out:
                 print("iter:{}, time:{}, control:{}".format(itr, end - start,
-                                         dict(zip(["x", "y", "z", "roll", "pitch", "yaw"], _future_pos[-6:]))))
+                                                            dict(zip(["x", "y", "z", "roll", "pitch", "yaw"],
+                                                                     _control_signal))))
+                print("force:{}".format(_force))
             itr = itr + 1
             if end - start >= _dur:
                 print("Reached time limit.")
@@ -65,11 +71,12 @@ if __name__ == "__main__":
     arm = initialize_arm(ip, 0)  # use position control mode
     set_to_init_pos(arm, speed=300)
     turn_on_force_sensor(arm)
+    # enable_online_mode(arm)
     while True:
         event = keyboard.read_event()
         print_out = True
         if event.event_type == keyboard.KEY_DOWN and event.name == 'enter':
-            dur = 10
-            deploy_model(arm, model, dur, 5, print_out)
+            dur = 20
+            deploy_model(arm, model, dur, 10, print_out)
         if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
             safe_exit(arm, 0)